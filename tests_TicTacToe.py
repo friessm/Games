d02fba21855e9ddcd26271a7ee82fa2ef42682fe
Marco Friess <marco.friess@gmail.com>
2020-06-04 22:41:12 +0200

    Adding test cases for is_winner method.

--- tests_TicTacToe.py
+++ tests_TicTacToe.py
@@ -10,6 +10,10 @@ class TicTacToeTest(unittest.TestCase):
         self.game = TicTacToe.TicTacToe()
 
     def test_move(self):      
+        
+        # TODO: How to supress the text to stdout?
+        print('testing move method ... \n')
+        
         # Test the happy cases
         for i in range(0,9):
             self.assertTrue(self.game.move(i, self.game.turn))
@@ -24,11 +28,43 @@ class TicTacToeTest(unittest.TestCase):
             self.assertFalse(self.game.move(i, self.game.turn))
 
     def test_is_winner(self):
-
-        t1 = [1, 0, 0, 1, 0, 0, 1, 0, 0]
         
+        print('testing is_winner method ... \n')
+        
+        # Positive cases
+        # Test rows and columns for player 1
+        tlist1 = [[1, 0, 0, 1, 0, 0, 1, 0, 0],
+                  [0, 1, 0, 0, 1, 0, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 1, 0, 0, 1],
+                  [1, 1, 1, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 1, 1, 1]]
 
+        for i in range(len(tlist1)):
+            self.game.board = tlist1[i]
+            self.assertTrue(self.game.is_winner())
 
+        # Test diagonals for player 2
+        self.game.next_turn()
+        tlist2 = [[2, 0, 0, 0, 2, 0, 0, 0, 2],
+                  [0, 0, 2, 0, 2, 0, 2, 0, 0]]
+
+        for i in range(len(tlist2)):
+            self.game.board = tlist2[i]
+            self.assertTrue(self.game.is_winner())
+
+        # Negative cases for player 1
+        self.game.next_turn()
+        tlist3 = [[0, 1, 0, 1, 0, 0, 1, 0, 0],
+                  [0, 0, 2, 0, 1, 0, 0, 1, 0],
+                  [0, 0, 2, 0, 0, 1, 0, 0, 1],
+                  [1, 1, 2, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 1, 2, 1, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, 1, 1, 2]]
+
+        for i in range(len(tlist3)):
+            self.game.board = tlist3[i]
+            self.assertFalse(self.game.is_winner())
 
 
 if __name__ == '__main__':
