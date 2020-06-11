53a5e271cd6087a329617fe79144daf19c840824
Marco Friess <marco.friess@gmail.com>
2020-06-11 21:35:25 +0200

    Final fixes to is_winner and final touches.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,5 @@
 
 from math import inf
-from itertools import groupby
 
 class TicTacToe:
     
@@ -12,7 +11,7 @@ class TicTacToe:
         self.scores = {'1': -10, '2': 10, '0': 0}
 
     def reset(self):
-        # Reset the game to the original position
+        """ Reset the game to the original position """
         self.board = [0 for i in range(9)]
 
     def render_board(self):
@@ -21,24 +20,28 @@ class TicTacToe:
         print('\n')
 
     def next_turn(self):
-        # Switch who's players turn it is.
+        """ Switch who's players turn it is. """
         if self.turn == self.human:
             self.turn = self.ai
         else:
             self.turn = self.human
 
     def move(self, act, player):
+        """ Make valid moves. """
         if self.board[act] == 0:
             self.board[act] = player
             return True
         else: 
-            # if player == 1: print('This is a non-empty field, idiot.')
             return False
+
+    def valid_moves(self):
+        """ Return list of valid moves. """
+        return [k for k, v in enumerate(self.board) if v == 0]
 
     def is_winner(self):
         """ Check columns, rows, the two diagonals and tie, or return None. """
         b = self.board
-        for turn in [1, 2]:
+        for turn in [self.human, self.ai]:
             for i in range(3):     
                 if all(x==turn for x in [b[i],b[i+3], b[i+6]]):
                     return turn
@@ -48,62 +51,76 @@ class TicTacToe:
                 return turn
             if all(x==turn for x in [b[2], b[4], b[6]]):
                 return turn
-            if 0 not in b:
-                return 0
-            else:
-                return None
+        if 0 not in b:
+            return 0
+        return None
 
-    def minimax(self, player):
+    def minimax(self, max_player):
+        """ Minimax to find optimal move for ai. """
         res = self.is_winner()
         if res != None:
             return self.scores[str(res)]
-        if self.turn == self.ai:
+        if max_player:
             value = -inf
-            for i in range(0, 9):
-                if self.move(i, self.turn):
-                    value = max(value, self.minimax(self.next_turn()))
+            for i in self.valid_moves():
+                if self.move(i, self.ai):
+                    value = max(value, self.minimax(False))
                     self.board[i] = 0
             return value
         else:
             value = inf
-            for i in range(0, 9):
-                if self.move(i, self.turn):
-                    value = min(value, self.minimax(self.next_turn()))
+            for i in self.valid_moves():
+                if self.move(i, self.human):
+                    value = min(value, self.minimax(True))
                     self.board[i] = 0
             return value
 
     def best_ai_act(self):
+        """ Initiate search for optimal ai move. """
         vacts = []
-        for i in range(0, 9):
+        for i in self.valid_moves():
             if self.move(i, self.turn):
-                vacts.append((self.minimax(self.next_turn()), i))
+                vacts.append((self.minimax(False), i))
                 self.board[i] = 0
         best_act = max(vacts, key=lambda item:item[0])
-        print(vacts)
-        print(best_act)
-        if self.turn == self.human:
-            self.next_turn()
         return best_act[1]
 
+    def get_human_act(self):
+        """ Get human player input. """
+        while True:
+            try:
+                # TODO: Handle all kinds of bad input
+                act = int(input('Player {} make your move: '.format(self.turn)))
+            except ValueError:
+                print('Invalid input. Valid input are integers from 0 to 8.')
+            if act >= 0 and act < 9:
+                if self.board[act] == 0:
+                    break
+                else:
+                    print('This is a non-empty field, idiot.')
+            else: 
+                print('Invalid input. Valid input are integers from 0 to 8.')
+        return act
+
     def run(self):
-        # Run the game
+        """ Run the game. """ 
         self.render_board()
         while True:      
             if self.turn == self.human:
                 while True:
-                    # TODO: Handle all kinds of bad input
-                    act = int(input('Player {} make your move: '.format(self.turn)))  
-                    if self.move(act, self.human):
+                    if self.move(self.get_human_act(), self.human):
                         break                                 
-            # AI's turn (maximizing player)
-            else:
-                act = self.best_ai_act()
-                print(self.turn)
-                self.move(act, self.turn)
+            else: # AI's turn (maximizing player)
+                self.move(self.best_ai_act(), self.turn)
 
             result = self.is_winner()
             if result != 0 and result != None:
-                print('And the winner is: Player {}'.format(self.turn))
+                print('And the winner is: Player {}\n'.format(self.turn))
+                self.render_board()
+                break
+
+            if result == 0:
+                print('Tie. No winner.')
                 self.render_board()
                 break
         
